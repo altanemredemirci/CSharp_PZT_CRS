8e25c894ba99055cd66618332e316cdeeabbab65
Altan Emre <altanemre1989@gmail.com>
2025-06-23 21:41:33 +0300

    If_Else

--- 04_StringMetotlar/Program.cs
+++ 04_StringMetotlar/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Altan Emre Demirci";
+
+            #region IndexOf
+            
+            //Index: Koleksiyonlar sahip oldukları elemanları Index adı verilen default olarak 0 dan başlayan ve 1'er 1'er artan numaralandırma yöntemi ile tuttarlar. 
+            //Koleksiyonlar birden fazla veriyi bir arada tutukları için istenilen değere ulaşmak indexs ile mümkündür.
+
+            //int indx = name.IndexOf("a"); // e karakterinin ilk bulunduğu index değeri gelir.
+            //Console.WriteLine(indx);
+
+
+
+        
+
+            #endregion
+
+
         }
     }
 }
---
+++ 05_KararYapilar/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05_KararYapilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
